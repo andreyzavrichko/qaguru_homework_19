4b806a21c287cd74ccd2efe8dd84f89ad6184555
a.zavrichko <andrey@zavrichko.dev>
2022-03-09 19:12:53 +0200

    create project

---
+++ src/test/java/ru/zavrichko/tests/BookstoreTests.java
@@ -0,0 +1,138 @@
+package ru.zavrichko.tests;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+import static ru.zavrichko.helpers.CustomAllureListener.withCustomTemplates;
+
+public class BookstoreTests {
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "https://demoqa.com";
+    }
+
+    @Test
+    void getBooksTest() {
+        get("/BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void getBooksWithAllLogsTest() {
+        given()
+                .log().all()
+                .when()
+                .get("/BookStore/v1/Books")
+                .then()
+                .log().all()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void getBooksWithSomeLogsTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void generateTokenTest() {
+        String data = "{ \"userName\": \"alex\", " +
+                "\"password\": \"asdsad#frew_DFS2\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."))
+                .body("token.size()", greaterThan(10));
+    }
+
+    @Test
+    void generateTokenWithAllureListenerTest() {
+        String data = "{ \"userName\": \"alex\", " +
+                "\"password\": \"asdsad#frew_DFS2\" }";
+
+//        RestAssured.filters(new AllureRestAssured()); in @BeforeAll
+        given()
+                .filter(new AllureRestAssured())
+                .contentType(JSON)
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."))
+                .body("token.size()", greaterThan(10));
+    }
+    @Test
+    void generateTokenWithCustomAllureListenerTest() {
+        String data = "{ \"userName\": \"alex\", " +
+                "\"password\": \"asdsad#frew_DFS2\" }";
+
+        given()
+                .filter(withCustomTemplates())
+                .contentType(JSON)
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("shemas/generateToken_response_shema.json"))
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."))
+                .body("token.size()", greaterThan(10));
+    }
+
+    @Test
+    void getBookTest() {
+        String data = "9781449325862";
+        given()
+                .filter(withCustomTemplates())
+                .contentType(JSON)
+               // .body(data)
+                .param("9781449325862")
+                .log().uri()
+                .log().body()
+                .when()
+                .get("/BookStore/v1/Book")
+                .then()
+                .log().status()
+                .log().body()
+                .body("title", is(equals("Git Pocket Guide")));
+    }
+
+}
